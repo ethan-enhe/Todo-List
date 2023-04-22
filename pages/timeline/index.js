4855a40221eaf4f81e5cb26d7e60389a566b927f
ethan <18201521626@139.com>
2023-04-22 20:23:12 +0800

    initial commit - 建立目录结构 - 完成tabbar（使用小程序原生的tabbar）

---
+++ pages/timeline/index.js
@@ -0,0 +1,83 @@
+// pages/timeline/index.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
+})
+
+Component({
+  pageLifetimes: {
+    show() {
+        /*
+        每个页面的tab栏实例是不一样的
+        在切换到的页面里还需要设置那个页面的tab实例的选中项目。
+        */
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 1
+        })
+      }
+    }
+  }
+})
