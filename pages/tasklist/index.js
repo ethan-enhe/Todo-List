5ecea95a0c14e2eaa2bc1c81785d7d76716cc7e5
ethan <18201521626@139.com>
2023-05-14 21:58:43 +0800

    修复timeline页面的tabbar 加了个gitignore

--- pages/tasklist/index.js
+++ pages/tasklist/index.js
@@ -41,7 +41,6 @@ Page({
 
     },
 
-
     /**
      * 生命周期函数--监听页面加载
      */
@@ -62,9 +61,18 @@ Page({
      * 生命周期函数--监听页面显示
      */
     onShow() {
-        app = getApp();
 
+
+        //每个页面的tab栏实例是不一样的
+        //在切换到的页面里还需要设置那个页面的tab实例的选中项目。
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 2
+        })
+      }
     },
+
 
     /**
      * 生命周期函数--监听页面隐藏
