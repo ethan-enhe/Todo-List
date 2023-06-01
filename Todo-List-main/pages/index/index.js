1c792daa2b079d2c33bfe6949ea1ec840854196a
yzyatghb <74448111+yzyatghb@users.noreply.github.com>
2023-06-01 23:29:34 +0800

    Add files via upload
    settings页面以及背景added

---
+++ Todo-List-main/pages/index/index.js
@@ -0,0 +1,22 @@
+Page({
+    onShow(){
+    },
+    onload(){
+    }
+})
+Component({
+  pageLifetimes: {
+    show() {
+        /*
+        每个页面的tab栏实例是不一样的
+        在切换到的页面里还需要设置那个页面的tab实例的选中项目。
+        */
+      if (typeof this.getTabBar === 'function' &&
+        this.getTabBar()) {
+        this.getTabBar().setData({
+          selected: 0
+        })
+      }
+    }
+  }
+})
