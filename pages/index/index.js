401063e2335e432661db0a09adc12b458c8a4913
中華棒球隊 <117569172+Petershen-csworld@users.noreply.github.com>
2023-06-04 12:12:31 +0800

    IP addr

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,14 @@
 Page({
-    onShow(){
-    },
+    
     onload(){
+      
+    },
+    data:{
+
+      ip:'1.1.1.1'
     }
-})
-Component({
-  pageLifetimes: {
-    show() {
+     ,
+    onShow() {
         /*
         每个页面的tab栏实例是不一样的
         在切换到的页面里还需要设置那个页面的tab实例的选中项目。
@@ -16,7 +18,18 @@ Component({
         this.getTabBar().setData({
           selected: 0
         })
-      }
+      };
+      const that = this;
+
+      wx.request({
+        url : "https://api64.ipify.org/?format=json",
+        success:function(e){
+          console.log(e.data.ip);
+          that.setData({ip:e.data.ip});
+          console.log(that.data.ip);
+        },
+
+   })
     }
   }
-})
+)
