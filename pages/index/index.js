c1575c5157c51d7e229e8c686a788ac67a940c16
中華棒球隊 <117569172+Petershen-csworld@users.noreply.github.com>
2023-06-04 18:22:54 +0800

    idx

--- pages/index/index.js
+++ pages/index/index.js
@@ -43,7 +43,7 @@ Page({
     },
     getmotto(){
       var  len = this.data.motto.length;
-      var idx = this.randomNum(0,len-1);
+      var idx = this.randomNum(-1,len-1);
        this.setData({curstr:this.data.motto[idx]})
     },
 
