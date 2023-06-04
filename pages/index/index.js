013a6a673bd72c8c52d47cf5da31017dc8997e51
中華棒球隊 <117569172+Petershen-csworld@users.noreply.github.com>
2023-06-04 12:55:12 +0800

    geolocate

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,16 +1,25 @@
 Page({
-    onShow(){
+    data:{
+      country:"China",
+      city:"Beijing",
+  
     },
-    onload(){
-    }
-})
-Component({
-  pageLifetimes: {
-    show() {
+    onShow() {
         /*
         每个页面的tab栏实例是不一样的
         在切换到的页面里还需要设置那个页面的tab实例的选中项目。
         */
+       const that = this;
+       wx.request(
+        {
+            url:"http://ip-api.com/json",
+            success(e){
+                that.setData({city:e.data.city});
+                that.setData({country:e.data.country})
+             }
+          }
+
+       )
       if (typeof this.getTabBar === 'function' &&
         this.getTabBar()) {
         this.getTabBar().setData({
@@ -19,4 +28,4 @@ Component({
       }
     }
   }
-})
+)
